adda9c27e64e6533c00703b545ea5081a09afdd3
ChewyB <Jb515764>
2019-08-19 18:34:15 -0700

    Data is displaying now

--- ProjectJB-SPA/src/components/Layout/Layout.js
+++ ProjectJB-SPA/src/components/Layout/Layout.js
@@ -2,10 +2,22 @@ import React from "react";
 import Navigation from "../Navigation/Navigation";
 
 const layout = props => {
+
+  var divStyle = {
+    display:'flex',
+    flexDirection:'row',
+    justifyContent: 'space-around'
+  };
+
   return (
     <>
       <Navigation />
       <main>{props.children}</main>
+      <div style={divStyle}>
+        <div>{props.name}</div>
+        <div>{props.id}</div>
+      </div>
+  
     </>
   );
 };
--- ProjectJB-SPA/src/containers/App.js
+++ ProjectJB-SPA/src/containers/App.js
@@ -9,26 +9,28 @@ import axios from "axios";
 
 class App extends Component {
   state = {
-    apiData: null
+    id: 0,
+    name: 'hello'
   };
 
-  componentWillMount() {
+  componentDidMount() {
     const url = "http://localhost:5000/api/values";
     axios
       .get(url)
       .then(response => response.data)
       .then(data => {
-        this.setState({ apiData: data });
-        //console.log(this.state.apiData);
+        this.setState({ 
+          id: data[0].id,
+          name: data[0].name
+        });
+         console.log(this.state.apiData);
       });
   }
 
   render() {
     return (
       <BrowserRouter>
-        <Layout>
-          {/* <p>{this.state.apiData.id}</p>
-          <p>{this.state.apiData.name}</p> */}
+        <Layout name={this.state.name} id={this.state.id}>
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/owner-list" component={OwnerList} />
